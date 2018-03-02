09085dc26f490348da2ca562897eaad4cd3145c8
Liam Ling <kla121@sfu.ca>
2018-03-01 23:28:24 -0800

    add files

---
+++ motd.sh
@@ -0,0 +1,10 @@
+artii "welcome" | lolcat
+fortune | cowsay -f vader | lolcat
+
+echo "-------------------------------------------" | lolcat
+uname -a | lolcat
+
+echo "-------------------------------------------" | lolcat
+(echo "CPU stats:" | lolcat ; istats cpu ;
+ echo "Fan stats:" | lolcat ; istats fan ;
+ echo "Battery stats:" | lolcat ; istats battery)
